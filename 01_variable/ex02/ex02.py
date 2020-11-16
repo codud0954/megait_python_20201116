6d2adffc5f48641e08a1b9b70537f0d88958dfe5
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-16 22:53:29 +0900

    Update ex02.py

---
+++ 01_variable/ex02/ex02.py
@@ -0,0 +1,28 @@
+# 변수와 데이터타입(Data type)의 종류
+
+# 정수형 변수(Integer)
+a = 10
+print(a) # 변수에 값을 넣고 변수를 출력하면 값이 나온다.
+
+# 변수이므로 값을 바꿀 수도 있다
+a = 5
+print(a)
+
+# 실수형 변수(Float)
+b = 1.5
+
+print(b)
+print(a * b) # 정수, 실수 연산이 가능하다
+
+
+# 문자열 변수(String)
+s1 = 'hello'
+s2 = 'world'
+print(s1, s2)
+
+# 참 또는 거짓(boolean)
+is_real = True
+print(is_real)
+
+is_real = False
+print(is_real)
