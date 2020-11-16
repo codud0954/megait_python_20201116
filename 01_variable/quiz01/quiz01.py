083fa7675896be67d36f2cc88c2f1987e009cba8
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-16 22:55:00 +0900

    Update quiz01.py

---
+++ 01_variable/quiz01/quiz01.py
@@ -0,0 +1,18 @@
+name = "신보람"
+age = 25
+
+print("이름:", name, "\n")
+print("나이:", age, "\n")
+
+a = 3
+b = 5
+print(a, "+", b, "=", (a + b))
+print(a, "-", b, "=", (a - b))
+print()  # 줄바꿈
+
+num = 7
+print(num, "X", 1, "=", (num * 1))
+print(num, "X", 2, "=", (num * 2))
+print(num, "X", 3, "=", (num * 3))
+print(num, "X", 4, "=", (num * 4))
+print(num, "X", 5, "=", (num * 5))
