86963ca2704db813397c7eedd3c268db5435c25a
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-23 20:55:30 +0900

    Update quiz01_1.py

---
+++ 02_condition/quiz01/quiz01_1.py
@@ -0,0 +1,10 @@
+alphabet = input("소문자 알파벳을 입력하세요: ")
+
+if alphabet < 'k':
+    print("%c는 k보다 앞에 있습니다." % alphabet)
+
+if alphabet > 'k':
+    print("%c는 k보다 뒤에 있습니다." % alphabet)
+    
+if alphabet == 'k':
+    print("%c는 k와 같다." % alphabet)
