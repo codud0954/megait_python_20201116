861f9d8f18915e1699c619bd50039363f4b1885f
marobiana <69413030+marobiana@users.noreply.github.com>
2020-11-27 20:55:51 +0900

    Update ex01.py

---
+++ 05_function/ex01/ex01.py
@@ -0,0 +1,14 @@
+# 길이를 구하는 함수
+length = len("python")
+print(length)
+print(len("python"))
+
+# tuple  (1, 2, 3)
+t1 = (1, 2, 3)
+length = len(t1)
+print(length)
+
+# divmod 몫과 나머지 구하기
+q, r = divmod(20, 3)
+print("몫:%d, 나머지:%d" % (q, r))
+print(divmod(20, 3))
