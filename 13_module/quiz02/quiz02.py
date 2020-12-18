2a8f2959737f8b777c914ca4d8996d850d509ab1
marobiana <69413030+marobiana@users.noreply.github.com>
2020-12-18 18:02:50 +0900

    Update quiz02.py

---
+++ 13_module/quiz02/quiz02.py
@@ -0,0 +1,7 @@
+from module.quiz02.quiz02_module import *
+
+print("add:", add(10, 5))
+print("sub:", sub(10, 5))
+print("mul:", mul(10, 5))
+print("div:", div(10, 5))
+print("remainder:", remainder(10, 5))
