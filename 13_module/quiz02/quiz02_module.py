a71f57d4332027406953599612cd014de2d26713
marobiana <69413030+marobiana@users.noreply.github.com>
2020-12-18 18:03:11 +0900

    Update quiz02_module.py

---
+++ 13_module/quiz02/quiz02_module.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x // y
+
+def remainder(x, y):
+    return x % y
+
+if __name__ == "__main__":
+    print(add(5, 10))
